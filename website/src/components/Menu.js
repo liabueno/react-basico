8ba30b5d1e6e57378f843611e7cac4cad19d2527
liabueno <juliavbueno13@gmail.com>
2024-09-10 08:57:39 -0300

    bottom e icon

--- website/src/components/Menu.js
+++ website/src/components/Menu.js
@@ -7,9 +7,10 @@ function Menu() {
   return (
     <nav>
       <ul className="menu">
+        <li><Link to="/"><img src='/logo.png' class="logo"></img></Link></li>
         <li><Link to="/"><FaHome /> Home</Link></li>
-        <li><Link to="/nasa"><FaRocket /> NASA</Link></li>
-        <li><Link to="/contact"><FaPhone /> Contact</Link></li>
+        <li><Link to="/nasa"><FaRocket /> APOD</Link></li>
+        <li><Link to="/contact"><FaPhone /> Contato</Link></li>
       </ul>
     </nav>
   );
